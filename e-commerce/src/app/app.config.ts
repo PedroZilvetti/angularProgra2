fb5f0082cac75f0c0c9c30e21d487d1bab596d5d
Pedro Zilvetti <pedropiloto@DESKTOP-I044SK0.localdomain>
2024-11-21 15:38:10 -0400

    añadi el firebase al proyecto segun el app.config del fer

--- e-commerce/src/app/app.config.ts
+++ e-commerce/src/app/app.config.ts
@@ -1,9 +1,28 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { CommonModule } from '@angular/common';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    importProvidersFrom(HttpClientModule, ReactiveFormsModule, AngularFireAuthModule, CommonModule),
+    provideFirebaseApp(() => initializeApp({
+      apiKey: "AIzaSyDq0-tuyOQqdFblXN3Hit4h_9XUm-EaCBM",
+      authDomain: "ecomerce-fc050.firebaseapp.com",
+      projectId: "ecomerce-fc050",
+      storageBucket: "ecomerce-fc050.firebasestorage.app",
+      messagingSenderId: "385513628433",
+      appId: "1:385513628433:web:cbd3823811bcebb58022b0"
+    })),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes)
+  ]
 };
