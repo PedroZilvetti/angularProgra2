f01e5094c09556c17b457392b8421d2515ffa706
Miguel Valeriano <jose.machaca.v@ucb.edu.bo>
2024-11-27 13:31:36 -0400

    Creo que ya pude arreglarlo, CREO y ESPERO

--- e-commerce/src/app/app.config.ts
+++ e-commerce/src/app/app.config.ts
@@ -2,7 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { RouterModule, Routes } from '@angular/router';
 
-import { FormsModule } from '@angular/forms'; 
+import { FormsModule } from '@angular/forms';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
@@ -10,9 +10,16 @@ import { getAuth, provideAuth } from '@angular/fire/auth';
 import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), 
-    provideFirebaseApp(() => initializeApp({"projectId":"ecomerce-fc050","appId":"1:385513628433:web:cbd3823811bcebb58022b0","storageBucket":"ecomerce-fc050.firebasestorage.app","apiKey":"AIzaSyDq0-tuyOQqdFblXN3Hit4h_9XUm-EaCBM","authDomain":"ecomerce-fc050.firebaseapp.com","messagingSenderId":"385513628433"})), 
-    provideAuth(() => getAuth()), 
-    provideFirestore(() => getFirestore())]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
+  provideFirebaseApp(() => initializeApp({
+    projectId: "ecomerce-fc050",
+    appId: "1:385513628433:web:cbd3823811bcebb58022b0",
+    storageBucket: "ecomerce-fc050.firebasestorage.app",
+    apiKey: "AIzaSyDq0-tuyOQqdFblXN3Hit4h_9XUm-EaCBM",
+    authDomain: "ecomerce-fc050.firebaseapp.com",
+    messagingSenderId: "385513628433"
+  })),
+  provideAuth(() => getAuth()),
+  provideFirestore(() => getFirestore())]
 };
 
--- e-commerce/src/pages/register/register.component.ts
+++ e-commerce/src/pages/register/register.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
+import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms'; // Importar NgForm si usas formularios template-driven
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   // styleUrls: ['./register.component.css']
+  imports: [FormsModule],
 })
 export class RegisterComponent {
   email: string;
